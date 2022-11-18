2f9302ccc87620cf70aa85fef583d748a062026c
AndreySHB <deerhunter9136@gmail.com>
2022-11-18 14:58:54 +0300

    5_5_HW4_parallel2.patch

--- persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/UserDao.java
@@ -29,7 +29,7 @@ public abstract class UserDao implements AbstractDao {
     @Transaction
     public int getSeqAndSkip(int step) {
         int id = getNextVal();
-        DBIProvider.getDBI().useHandle(h -> h.execute("ALTER SEQUENCE user_seq RESTART WITH " + (id + step)));
+        DBIProvider.getDBI().useHandle(h -> h.execute(String.format("SELECT setval('user_seq', %d)", id + step -1)));
         return id;
     }
 
